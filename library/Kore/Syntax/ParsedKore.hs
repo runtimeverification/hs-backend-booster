9aaf7fceab95ee053efb5244aa3f3be06080fd32
Jost Berthold <jost.berthold@gmail.com>
2022-11-04 11:00:29 +1100

    Parsed Definition types (WIP)

---
+++ library/Kore/Syntax/ParsedKore.hs
@@ -0,0 +1,84 @@
+{- |
+Copyright   : (c) Runtime Verification, 2022
+License     : BSD-3-Clause
+
+@ParsedKore@ models a K definition (set of modules) that has been
+parsed (but not validated). It uses the types @KorePattern@ and its
+components from the Json interface module, and derives a Json
+representation.
+
+See https://github.com/runtimeverification/haskell-backend/blob/master/docs/kore-syntax.md
+for a description of the syntax itself.
+
+-}
+
+module Kore.Syntax.ParsedKore (
+    module Kore.Syntax.ParsedKore,
+) where
+
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Text (Text)
+import Deriving.Aeson (CustomJSON(..))
+import GHC.Generics (Generic)
+
+import Kore.Syntax.Json.Base qualified as Json
+
+data ParsedDefinition
+    = ParsedDefinition
+      { modules :: [ ParsedModule ]
+      , attributes :: ParsedAttributes
+      }
+    deriving stock (Eq, Show, Generic)
+    deriving (FromJSON, ToJSON) via CustomJSON '[] ParsedDefinition
+
+data ParsedModule
+    = ParsedModule
+      { name :: Json.Id
+      -- , imports :: [(Json.Id, ParsedAttributes)]
+      , sorts :: [ParsedSort]
+      , symbols :: [ParsedSymbol]
+      -- , aliases :: [ParsedAlias]
+      , axioms :: [ParsedAxiom]
+      -- , claims :: [ParsedClaim]
+      }
+    deriving stock (Eq, Show, Generic)
+    deriving (FromJSON, ToJSON) via CustomJSON '[] ParsedModule
+
+data ParsedSort
+    = ParsedSort
+      { name :: Json.Id
+      , sortVars :: [ Json.Id ]
+      , isHooked :: Bool
+      , attributes :: ParsedAttributes
+      }
+    deriving stock (Eq, Show, Generic)
+    deriving (FromJSON, ToJSON) via CustomJSON '[] ParsedSort
+
+data ParsedSymbol
+    = ParsedSymbol
+      { name :: Json.Id
+      , argSorts :: [Json.Sort]
+      , sort :: Json.Sort
+      , isHooked :: Bool
+      , attributes :: ParsedAttributes
+      }
+    deriving stock (Eq, Show, Generic)
+    deriving (FromJSON, ToJSON) via CustomJSON '[] ParsedSymbol
+
+-- data ParsedAlias = ... (ParsedSymbol with additional patterns, without "hooked")
+
+data ParsedAxiom
+    = ParsedAxiom
+      { axiom :: Json.KorePattern  -- assumed to have certain shape
+                                   -- (not validated here)
+      , sortVars :: [ Json.Id ]
+      , attributes :: ParsedAttributes
+      }
+    deriving stock (Eq, Show, Generic)
+    deriving (FromJSON, ToJSON) via CustomJSON '[] ParsedAxiom
+
+-- data ParsedClaim = ... (same as ParsedAxiom)
+
+type ParsedAttributes = [(AttributeName, AttributeValue)]
+type AttributeName = Json.Id
+type AttributeValue = Text
