545321e49028580c680e1bc8a50bbd9c54741dec
ana-pantilie <ana.pantilie95@gmail.com>
2022-11-07 16:26:53 +0200

    Add server infrastructure

--- library/Kore/JsonRpc/Base.hs
+++ library/Kore/JsonRpc/Base.hs
@@ -41,32 +41,12 @@ data ExecuteRequest = ExecuteRequest
         (FromJSON)
         via CustomJSON '[OmitNothingFields, FieldLabelModifier '[CamelToKebab]] ExecuteRequest
 
-data ImpliesRequest = ImpliesRequest
-    { antecedent :: !KoreJson
-    , consequent :: !KoreJson
-    }
-    deriving stock (Generic, Show, Eq)
-    deriving
-        (FromJSON)
-        via CustomJSON '[OmitNothingFields, FieldLabelModifier '[CamelToKebab]] ImpliesRequest
-
-newtype SimplifyRequest = SimplifyRequest
-    { state :: KoreJson
-    }
-    deriving stock (Generic, Show, Eq)
-    deriving
-        (FromJSON)
-        via CustomJSON '[OmitNothingFields, FieldLabelModifier '[CamelToKebab]] SimplifyRequest
-
 data ReqException = CancelRequest deriving stock (Show)
 
 instance Exception ReqException
 
 instance FromRequest (API 'Req) where
     parseParams "execute" = Just $ fmap (Execute <$>) parseJSON
-    -- parseParams "step" = Just $ fmap (Step <$>) parseJSON
-    parseParams "implies" = Just $ fmap (Implies <$>) parseJSON
-    parseParams "simplify" = Just $ fmap (Simplify <$>) parseJSON
     parseParams "cancel" = Just $ const $ return Cancel
     parseParams _ = Nothing
 
@@ -103,32 +83,13 @@ data ExecuteResult = ExecuteResult
         (ToJSON)
         via CustomJSON '[OmitNothingFields, FieldLabelModifier '[CamelToKebab]] ExecuteResult
 
-data Condition = Condition
-    { substitution :: KoreJson
-    , predicate :: KoreJson
+newtype Condition = Condition
+    { predicate :: KoreJson
     }
     deriving stock (Generic, Show, Eq)
     deriving
         (ToJSON)
         via CustomJSON '[OmitNothingFields, FieldLabelModifier '[CamelToKebab]] Condition
-
-data ImpliesResult = ImpliesResult
-    { implication :: KoreJson
-    , satisfiable :: Bool
-    , condition :: Maybe Condition
-    }
-    deriving stock (Generic, Show, Eq)
-    deriving
-        (ToJSON)
-        via CustomJSON '[OmitNothingFields, FieldLabelModifier '[CamelToKebab]] ImpliesResult
-
-newtype SimplifyResult = SimplifyResult
-    { state :: KoreJson
-    }
-    deriving stock (Generic, Show, Eq)
-    deriving
-        (ToJSON)
-        via CustomJSON '[OmitNothingFields, FieldLabelModifier '[CamelToKebab]] SimplifyResult
 
 data ReqOrRes = Req | Res
 
@@ -140,18 +101,9 @@ data APIMethods
 type family APIPayload (api :: APIMethods) (r :: ReqOrRes) where
     APIPayload 'ExecuteM 'Req = ExecuteRequest
     APIPayload 'ExecuteM 'Res = ExecuteResult
-    -- APIPayload 'StepM 'Req = StepRequest
-    -- APIPayload 'StepM 'Res = StepResult
-    APIPayload 'ImpliesM 'Req = ImpliesRequest
-    APIPayload 'ImpliesM 'Res = ImpliesResult
-    APIPayload 'SimplifyM 'Req = SimplifyRequest
-    APIPayload 'SimplifyM 'Res = SimplifyResult
 
 data API (r :: ReqOrRes) where
     Execute :: APIPayload 'ExecuteM r -> API r
-    -- Step :: APIPayload 'StepM r -> API r
-    Implies :: APIPayload 'ImpliesM r -> API r
-    Simplify :: APIPayload 'SimplifyM r -> API r
     Cancel :: API 'Req
 
 deriving stock instance Show (API 'Req)
@@ -162,13 +114,8 @@ deriving stock instance Eq (API 'Res)
 instance ToJSON (API 'Res) where
     toJSON = \case
         Execute payload -> toJSON payload
-        -- Step payload -> toJSON payload
-        Implies payload -> toJSON payload
-        Simplify payload -> toJSON payload
 
 instance Pretty.Pretty (API 'Req) where
     pretty = \case
         Execute _ -> "execute"
-        Implies _ -> "implies"
-        Simplify _ -> "simplify"
         Cancel -> "cancel"
