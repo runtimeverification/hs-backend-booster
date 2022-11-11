657a031313e38ccd774619b2fdbefe0931608d9d
Jost Berthold <jost.berthold@gmail.com>
2022-11-11 20:03:46 +1100

    More draft code for rpc test client

--- tools/rpc-client/RpcClient.hs
+++ tools/rpc-client/RpcClient.hs
@@ -14,40 +14,66 @@ module RpcClient (
     main,
 ) where
 
+import Control.Monad
 import Data.Aeson qualified as Json
-import Data.Conduit.Network (serverSettings, clientSettings)
-import Data.Text.IO qualified as Text
-import Network.JSONRPC
+import Data.ByteString.Lazy.Char8 qualified as BS
+import Network.Run.TCP
+import Network.Socket
+import Network.Socket.ByteString.Lazy
 import Options.Applicative
+import System.Exit
 
 import Kore.JsonRpc.Base qualified as Rpc
 import Kore.Syntax.Json.Base qualified as Syntax
 
+import Debug.Trace
+
 main :: IO ()
 main = do
     error "implement me!"
-    Options{host, port} <- execParser parseOptions
-    result <-
-        jsonrpcTCPClient
-            V2
-            True {- ignore-incoming -}
-            (clientSettings host port) $
-            action  -- the action
-  where
-    action = undefined
+    Options{host, port, action, optionFile, options, expectFile} <- execParser parseOptions
+    request <-
+        trace "Preparing request data" $
+        prepareRequestData action optionFile options
+    result <- runTCPClient host (show port) $ \s -> do
+        trace "Sending request..." $
+            sendAll s request
+        response <- recv s 8192
+        trace "Response received." $
+            pure response
+    maybe BS.putStrLn compareToExpectation expectFile $ result
 
 data Options =
     Options
-    { host :: Text
+    { host :: String
     , port :: Int
-    , raw :: Bool -- whether to send the file data verbatim or use the below
-    , verb :: Action -- what to do
+    , action :: Action -- what to do
     , optionFile :: Maybe FilePath -- file with options (different for each endpoint
-    , options :: [(Text, Text)] -- verbatim options (name, value) to add to json
+    , options :: [(String, String)] -- verbatim options (name, value) to add to json
     , expectFile :: Maybe FilePath -- whether to diff to an expectation file or output
     }
 
-data Action = Exec FilePath |  Simpl FilePath | Check FilePath -- the endpoints
+-- | Defines what to do. Either one of the endpoints, or raw data
+data Action
+    = Exec FilePath
+    | Simpl FilePath
+    | Check FilePath
+    | SendRaw FilePath
 
-parseOptions :: Parser Options
+parseOptions :: ParserInfo Options
 parseOptions = undefined
+
+prepareRequestData :: Action -> Maybe FilePath -> [(String, String)] -> IO BS.ByteString
+prepareRequestData (SendRaw file) _ _ =
+    BS.readFile file
+prepareRequestData (Exec file) opts mbOptFile = do
+    undefined
+
+compareToExpectation :: FilePath -> BS.ByteString -> IO ()
+compareToExpectation expectFile output = do
+    expected <- BS.readFile expectFile
+    -- TODO https://hackage.haskell.org/package/Diff
+    when (output /= expected) $ do
+        BS.putStrLn output
+        BS.putStrLn "Not the same, sorry."
+        exitWith $ ExitFailure 1
