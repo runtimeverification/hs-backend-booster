ba68bb554b0ec039a57435cd8943e423efb39e94
F-WRunTime <Freeman.Wenzl@runtimeverification.com>
2022-11-29 05:54:27 -0700

    Testing usign wtih-docker

---
+++ .github/workflows/Dockerfile.nix
@@ -0,0 +1,22 @@
+ARG BASE_OS
+ARG BASE_DISTRO
+
+FROM $BASE_OS:$BASE_DISTRO
+
+ENV TZ America/Chicago
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN    apt-get update        \
+    && apt-get install --yes \
+        curl                 \
+        git                  \
+        xz-utils
+
+# The image is built specifically for an environment with this user/group
+ARG USER=github-user
+ARG GROUP=$USER
+ARG USER_ID=1000
+ARG GROUP_ID=$USER_ID
+RUN groupadd -g $GROUP_ID $GROUP && useradd -m -u $USER_ID -s /bin/sh -g $GROUP $USER
+
+USER $USER:$GROUP
