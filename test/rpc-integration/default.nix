07930319f9c15334a7903fac877f7d4a091304e1
Sam Balco <goodlyrottenapple@gmail.com>
2023-01-18 15:45:19 +0000

    add initial nix test

--- test/rpc-integration/default.nix
+++ test/rpc-integration/default.nix
@@ -1,14 +1,20 @@
-{nixosTest} : 
-nixosTest {
-  system = "x86_64-linux";
-  name = "useless-test";
-  # `foo` will be referenced in the test script
-  nodes.foo = { ... }: {
-    environment.systemPackages = [ pkgs.cowsay ];
-  };
-  # The test script, written in Python
-  testScript = ''
-    foo.start() # or start_all() for starting all nodes
-    foo.succeed("cowthink $(whoami)")
+{ stdenv, hs-backend-booster, rpc-client, git }:
+stdenv.mkDerivation {
+  name = "rpc-integration";
+  src = ./.;
+  preferLocalBuild = true;
+  buildInputs = [ git ];
+
+  enableParallelBuilding = true;
+  buildPhase = ''
+    export SERVER=${hs-backend-booster}/bin/hs-backend-booster
+    export CLIENT=${rpc-client}/bin/rpc-client
+    patchShebangs runDirectoryTest.sh
+    ./runDirectoryTest.sh test-a-to-f
+  '';
+  installPhase = ''
+    runHook preInstall
+    touch "$out"
+    runHook postInstall
   '';
 }
