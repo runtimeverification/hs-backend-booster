ead788ae887cdaa1d59c07dea8c835c69213f051
Jost Berthold <jost.berthold@gmail.com>
2023-01-30 15:09:03 +1100

    basic scaffolding for LLVM simplification unit tests
    * Test definition  in
    * gets compiled in LLVM c mode before all LLVM simplification tests (once)
    * compilation output set to  (enables keeping prior output), .gitignore-d

--- test-suite/Test/Kore/LLVM.hs
+++ test-suite/Test/Kore/LLVM.hs
@@ -3,12 +3,12 @@ Copyright   : (c) Runtime Verification, 2023
 License     : BSD-3-Clause
 -}
 module Test.Kore.LLVM (
-    test_llvmLoad,
+    test_llvmSimplification,
 ) where
 
 import Data.List (isPrefixOf, isSuffixOf)
+import Data.Text (pack)
 import GHC.IO.Exception
-import System.Environment
 import System.FilePath
 import System.Process
 import Test.Hspec
@@ -18,8 +18,8 @@ import Test.Tasty.Hspec
 import Kore.LLVM.Internal
 
 -- A prerequisite for all tests in this suite is that a fixed K
--- definition was compiled for LLVM in 'c' mode to produce a dynamic
--- library, and that library is available under 'test/llvm.dl'
+-- definition was compiled in LLVM 'c' mode to produce a dynamic
+-- library, and is available under 'test/llvm-kompiled/interpreter'
 
 definition, kompiledPath, dlPath :: FilePath
 definition = "test/llvm/llvm.k"
@@ -28,21 +28,31 @@ dlPath = kompiledPath </> "interpreter"
 
 -- TODO integrate with hedgehog: https://hackage.haskell.org/package/hspec-hedgehog
 
-test_llvmLoad :: IO TestTree
-test_llvmLoad = testSpec "LLVM simplification library tests" llvmSpec
-
+test_llvmSimplification :: IO TestTree
+test_llvmSimplification = testSpec "LLVM simplification library tests" llvmSpec
 
 llvmSpec :: Spec
 llvmSpec =
     beforeAll_ runKompile $ do
-        describe "Load an LLVM simplification library" $ do
-            it "fails to load a non-existing library" $
-                withDLib "does/not/exist.dl" mkAPI `shouldThrow`
-                    \IOError{ioe_description = msg} ->
-                        "dlopen: does/not/exist" `isPrefixOf` msg
-                            && "No such file or directory" `isSuffixOf` msg
-            it ("loads a valid library from " <> dlPath) $ do
-                pendingWith "need compilation setup and env. variable"
+        llvmLoad
+
+llvmLoad :: Spec
+llvmLoad =
+    describe "Load an LLVM simplification library" $ do
+        it "fails to load a non-existing library" $
+            withDLib "does/not/exist.dl" mkAPI
+                `shouldThrow` \IOError{ioe_description = msg} ->
+                    "dlopen: does/not/exist"
+                        `isPrefixOf` msg
+                        && "No such file or directory"
+                        `isSuffixOf` msg
+        it ("loads a valid library from " <> dlPath) $ do
+            withDLib dlPath $ \dl -> do
+                api <- mkAPI dl
+                let testString = "testing, one, two, three"
+                s <- api.patt.string.new $ pack testString
+                api.patt.dump s
+                    `shouldReturn` show testString
 
 runKompile :: IO ()
 runKompile = do
