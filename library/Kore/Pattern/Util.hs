190696bbbf7ae15808e11cb3235d8310d1deeb30
Jost Berthold <jost.berthold@gmail.com>
2023-02-20 12:48:24 +1100

    Avoid traversing irrelevant config in `substituteInTerm`
    `substituteInTerm` was traversing all parts of the term that contains variables, regardless of what it was actually substituting. The change avoids traversal when the contained variables are not the substituted ones. This matters for large configurations like the KEVM one, but did not show in the IMP examples.
    
    Had to use an ignore pragma for hlint because of [an hlint bug with `OverloadedRecordDot`](https://github.com/ndmitchell/hlint/issues/1458)

--- library/Kore/Pattern/Util.hs
+++ library/Kore/Pattern/Util.hs
@@ -54,11 +54,13 @@ retractPattern :: TermOrPredicate -> Maybe Pattern
 retractPattern (TermAndPredicate patt) = Just patt
 retractPattern _ = Nothing
 
+{-# HLINT ignore substituteInTerm "Redundant bracket" #-}
 substituteInTerm :: Map Variable Term -> Term -> Term
 substituteInTerm substitution = goSubst
   where
+    targetSet = Map.keysSet substitution
     goSubst t
-        | Set.null (getAttributes t).variables = t
+        | Set.null (targetSet `Set.intersection` (getAttributes t).variables) = t
         | otherwise = case t of
             Var v -> fromMaybe t (Map.lookup v substitution)
             DomainValue{} -> t
