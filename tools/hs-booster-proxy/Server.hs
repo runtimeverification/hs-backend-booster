6dda715a166c738789039ebc0ffae4864f09879c
Sam Balco <goodlyrottenapple@gmail.com>
2023-03-06 16:18:17 +0000

    Add hs-booster-proxy

---
+++ tools/hs-booster-proxy/Server.hs
@@ -0,0 +1,309 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+
+{- |
+Copyright   : (c) Runtime Verification, 2022
+License     : BSD-3-Clause
+-}
+module Main (main) where
+
+import Control.Concurrent.MVar (newMVar)
+import Control.Concurrent.MVar qualified as MVar
+import Control.DeepSeq (force)
+import Control.Exception (evaluate, SomeException)
+import Control.Monad (forM_, void)
+import Control.Monad.Catch (bracket)
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import Control.Monad.Logger (LogLevel (..), MonadLoggerIO (askLoggerIO), LoggingT (runLoggingT), defaultLoc, ToLogStr (toLogStr), logInfoN)
+import Control.Monad.Logger qualified as Logger
+import Data.Conduit.Network (serverSettings)
+import Data.IORef (writeIORef)
+import Data.InternedText (globalInternedTextCache)
+import Data.List (intercalate, partition)
+import Data.Map qualified as Map
+import Data.Maybe (fromMaybe)
+import Data.Text (Text, pack)
+import Options.Applicative
+import System.Clock (
+    Clock (..),
+    getTime,
+ )
+import Text.Casing
+import Text.Read
+
+import Booster.LLVM.Internal (mkAPI, withDLib)
+import Booster.Syntax.ParsedKore (loadDefinition)
+import Booster.Trace
+import Booster.VersionInfo (VersionInfo (..), versionInfo)
+import GlobalMain qualified
+import Kore.Attribute.Symbol (StepperAttributes)
+import Kore.BugReport (BugReportOption (..), withBugReport)
+import Kore.IndexedModule.MetadataTools (SmtMetadataTools)
+import Kore.Internal.TermLike (TermLike, VariableName)
+import Kore.JsonRpc.Server
+import Kore.JsonRpc (ServerState (..), serverError)
+import Kore.JsonRpc.Types (rpcJsonConfig, API, ReqOrRes (Req, Res))
+import Kore.Log (ExeName (..), defaultKoreLogOptions, swappableLogger, withLogger, TimestampsSwitch (TimestampsDisable), KoreLogType (LogSomeAction), LogAction (LogAction))
+import Kore.Log qualified as Log
+import Kore.Rewrite.SMT.Lemma (declareSMTLemmas)
+import Kore.Syntax.Definition (ModuleName (ModuleName), SentenceAxiom)
+import Options.SMT (KoreSolverOptions (..), parseKoreSolverOptions)
+import Proxy qualified
+import SMT qualified
+import qualified Data.Text as Text
+import qualified Booster.JsonRpc as Booster
+import qualified Kore.JsonRpc as Kore
+import Data.Aeson (toJSON)
+import Proxy (KoreServer(..))
+
+main :: IO ()
+main = do
+    startTime <- getTime Monotonic
+    options <- execParser clParser
+    let CLOptions{definitionFile, mainModuleName, port, logLevels, llvmLibraryFile, eventlogEnabledUserEvents} = options
+        (logLevel, customLevels) = adjustLogLevels logLevels
+        levelFilter :: Logger.LogSource -> LogLevel -> Bool
+        levelFilter _source lvl =
+            lvl `elem` customLevels || lvl >= logLevel && lvl <= LevelError
+        
+    Logger.runStderrLoggingT $ Logger.filterLogger levelFilter $ do
+    
+        liftIO $ forM_ eventlogEnabledUserEvents $ \t -> do
+            putStrLn $ "Tracing " <> show t
+            enableCustomUserEvent t
+        Logger.logInfoNS "proxy" $ Text.pack $
+            "Loading definition from "
+                <> definitionFile
+                <> ", main module "
+                <> show mainModuleName
+        definition <- liftIO $
+            loadDefinition mainModuleName definitionFile
+                >>= evaluate . force . either (error . show) id
+
+
+        monadLogger <- askLoggerIO
+
+        let coLogLevel = fromMaybe Log.Info $ toSeverity logLevel
+            koreLogOptions =
+                (defaultKoreLogOptions (ExeName $ "") startTime){
+                    Log.logLevel = coLogLevel,
+                    Log.timestampsSwitch = TimestampsDisable,
+                    Log.logType = LogSomeAction $ LogAction $ \txt -> liftIO $ monadLogger defaultLoc "kore" logLevel $ toLogStr txt
+                    }
+            srvSettings = serverSettings port "*"
+
+        liftIO $ void $ withBugReport (ExeName "hs-booster-proxy") BugReportOnError $ \reportDirectory ->
+            withLogger reportDirectory koreLogOptions $ \actualLogAction -> do
+                mvarLogAction <- newMVar actualLogAction
+                let logAction = swappableLogger mvarLogAction
+
+                kore@KoreServer{runSMT} <- mkKoreServer Log.LoggerEnv{logAction} options
+
+                withMDLib llvmLibraryFile $ \mdl -> do
+                    mLlvmLibrary <- maybe (pure Nothing) (fmap Just . mkAPI) mdl
+
+                    let booster =
+                            Proxy.BoosterServer
+                                { definition
+                                , mLlvmLibrary
+                                }
+
+                    runLoggingT (Logger.logInfoNS "proxy" "Starting RPC server") monadLogger
+
+                    -- Proxy.runServer port kore booster
+                    let koreRespond :: Respond (API 'Req) (LoggingT IO) (API 'Res)
+                        koreRespond = Kore.respond kore.serverState (ModuleName kore.mainModule) runSMT
+                        boosterRespond = Booster.respond booster.definition booster.mLlvmLibrary
+                        server =
+                            jsonRpcServer rpcJsonConfig srvSettings
+                                (const $ Proxy.respondEither boosterRespond koreRespond)
+                                [JsonRpcHandler $ \(err :: SomeException) -> logInfoN (Text.pack $ show err) >> pure (serverError "crashed" $ toJSON $ show err)]
+                    runLoggingT server monadLogger
+  where
+    clParser =
+        info
+            (clOptionsParser <**> versionInfoParser <**> helper)
+            parserInfoModifiers
+
+    withMDLib Nothing f = f Nothing
+    withMDLib (Just fp) f = withDLib fp $ \dl -> f (Just dl)
+
+toSeverity :: LogLevel -> Maybe Log.Severity
+toSeverity LevelDebug = Just Log.Debug
+toSeverity LevelInfo = Just Log.Info
+toSeverity LevelWarn = Just Log.Warning
+toSeverity LevelError = Just Log.Error
+toSeverity LevelOther{} = Nothing
+
+data CLOptions = CLOptions
+    { definitionFile :: FilePath
+    , mainModuleName :: Text
+    , llvmLibraryFile :: Maybe FilePath
+    , port :: Int
+    , logLevels :: [LogLevel]
+    , eventlogEnabledUserEvents :: [CustomUserEventType]
+    , koreSolverOptions :: !KoreSolverOptions
+    }
+
+parserInfoModifiers :: InfoMod options
+parserInfoModifiers =
+    fullDesc
+        <> header "Haskell Backend Booster - a JSON RPC server for quick symbolic execution of Kore definitions"
+
+versionInfoParser :: Parser (a -> a)
+versionInfoParser =
+    infoOption
+        versionInfoStr
+        ( short 'v'
+            <> long "version"
+            <> help "Print version info."
+        )
+
+clOptionsParser :: Parser CLOptions
+clOptionsParser =
+    CLOptions
+        <$> strArgument
+            ( metavar "DEFINITION_FILE"
+                <> help "Kore definition file to verify and use for execution"
+            )
+        <*> strOption
+            ( metavar "MODULE"
+                <> long "module"
+                <> help "The name of the main module in the Kore definition."
+            )
+        <*> optional
+            ( strOption
+                ( metavar "LLVM_BACKEND_LIBRARY"
+                    <> long "llvm-backend-library"
+                    <> help "Path to the .so/.dylib shared LLVM backend library"
+                )
+            )
+        <*> option
+            auto
+            ( metavar "SERVER_PORT"
+                <> long "server-port"
+                <> value 31337
+                <> help "Port for the RPC server to bind to"
+                <> showDefault
+            )
+        <*> many
+            ( option
+                (eitherReader readLogLevel)
+                ( metavar "LEVEL"
+                    <> long "log-level"
+                    <> short 'l'
+                    <> help
+                        ( "Log level: debug, info (default), warn, error, \
+                          \or a custom level from "
+                            <> show allowedLogLevels
+                        )
+                )
+            )
+        <*> many
+            ( option
+                (eitherReader readEventLogTracing)
+                ( metavar "TRACE"
+                    <> long "trace"
+                    <> short 't'
+                    <> help
+                        ( "Eventlog tracing options: "
+                            <> intercalate ", " [toKebab $ fromHumps $ show t | t <- [minBound .. maxBound] :: [CustomUserEventType]]
+                        )
+                )
+            )
+        <*> parseKoreSolverOptions
+  where
+    readLogLevel :: String -> Either String LogLevel
+    readLogLevel = \case
+        "debug" -> Right LevelDebug
+        "info" -> Right LevelInfo
+        "warn" -> Right LevelWarn
+        "error" -> Right LevelError
+        other
+            | other `elem` allowedLogLevels -> Right (LevelOther $ pack other)
+            | otherwise -> Left $ other <> ": Unsupported log level"
+
+    readEventLogTracing :: String -> Either String CustomUserEventType
+    readEventLogTracing =
+        (\s -> maybe (Left $ s <> " not supported in eventlog tracing") Right $ readMaybe s) . toPascal . fromKebab
+
+-- custom log levels that can be selected
+allowedLogLevels :: [String]
+allowedLogLevels = ["Rewrite"]
+
+-- Partition provided log levels into standard and custom ones, and
+-- select the lowest standard level. Default to 'LevelInfo' if no
+-- standard log level was given.
+adjustLogLevels :: [LogLevel] -> (LogLevel, [LogLevel])
+adjustLogLevels ls = (standardLevel, customLevels)
+  where
+    (stds, customLevels) = partition (<= LevelError) ls
+    standardLevel = if null stds then LevelInfo else minimum stds
+
+versionInfoStr :: String
+versionInfoStr =
+    unlines
+        [ "hs-backend-booster version:"
+        , "  revision:\t" <> gitHash <> if gitDirty then " (dirty)" else ""
+        , "  branch:\t" <> fromMaybe "<unknown>" gitBranch
+        , "  last commit:\t" <> gitCommitDate
+        ]
+  where
+    VersionInfo{gitHash, gitDirty, gitBranch, gitCommitDate} = $versionInfo
+
+mkKoreServer :: Log.LoggerEnv IO -> CLOptions -> IO Proxy.KoreServer
+mkKoreServer loggerEnv@Log.LoggerEnv{logAction} CLOptions{definitionFile, mainModuleName, koreSolverOptions} =
+    flip Log.runLoggerT logAction $ do
+        sd@GlobalMain.SerializedDefinition{internedTextCache} <-
+            GlobalMain.deserializeDefinition
+                koreSolverOptions
+                definitionFile
+                (ModuleName mainModuleName)
+        liftIO $ writeIORef globalInternedTextCache internedTextCache
+
+        loadedDefinition <- GlobalMain.loadDefinitions [definitionFile]
+        serverState <-
+            liftIO $
+                MVar.newMVar
+                    ServerState
+                        { serializedModules = Map.singleton (ModuleName mainModuleName) sd
+                        , loadedDefinition
+                        }
+
+        pure $
+            Proxy.KoreServer
+                { serverState
+                , mainModule = mainModuleName
+                , runSMT
+                , loggerEnv
+                }
+  where
+    KoreSolverOptions{timeOut, rLimit, resetInterval, prelude} = koreSolverOptions
+    smtConfig =
+        SMT.defaultConfig
+            { SMT.timeOut = timeOut
+            , SMT.rLimit = rLimit
+            , SMT.resetInterval = resetInterval
+            , SMT.prelude = prelude
+            }
+
+    -- SMT solver with user declared lemmas
+    runSMT ::
+        forall a.
+        SmtMetadataTools StepperAttributes ->
+        [SentenceAxiom (TermLike VariableName)] ->
+        SMT.SMT a ->
+        IO a
+    runSMT metadataTools lemmas m =
+        flip Log.runLoggerT logAction $
+            bracket (SMT.newSolver smtConfig) SMT.stopSolver $ \refSolverHandle -> do
+                let userInit = SMT.runWithSolver $ declareSMTLemmas metadataTools lemmas
+                    solverSetup =
+                        SMT.SolverSetup
+                            { userInit
+                            , refSolverHandle
+                            , config = smtConfig
+                            }
+                SMT.initSolver solverSetup
+                SMT.runWithSolver m solverSetup
