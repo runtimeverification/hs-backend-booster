e420506132510d303024657eb84e448574df7f49
Sam Balco <goodlyrottenapple@gmail.com>
2023-03-07 16:11:55 +0000

    factor out common command line options into Booster.CLOptions

--- tools/hs-booster-proxy/Server.hs
+++ tools/hs-booster-proxy/Server.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
 {- |
@@ -20,23 +19,18 @@ import Control.Monad.Logger qualified as Logger
 import Data.Conduit.Network (serverSettings)
 import Data.IORef (writeIORef)
 import Data.InternedText (globalInternedTextCache)
-import Data.List (intercalate, partition)
 import Data.Map qualified as Map
 import Data.Maybe (fromMaybe)
-import Data.Text (Text, pack)
 import Options.Applicative
 import System.Clock (
     Clock (..),
     getTime,
  )
-import Text.Casing
-import Text.Read
 
 import Booster.JsonRpc qualified as Booster
 import Booster.LLVM.Internal (mkAPI, withDLib)
 import Booster.Syntax.ParsedKore (loadDefinition)
 import Booster.Trace
-import Booster.VersionInfo (VersionInfo (..), versionInfo)
 import Data.Aeson (toJSON)
 import Data.Text qualified as Text
 import GlobalMain qualified
@@ -56,12 +50,13 @@ import Options.SMT (KoreSolverOptions (..), parseKoreSolverOptions)
 import Proxy (KoreServer (..))
 import Proxy qualified
 import SMT qualified
+import Booster.CLOptions
 
 main :: IO ()
 main = do
     startTime <- getTime Monotonic
     options <- execParser clParser
-    let CLOptions{definitionFile, mainModuleName, port, logLevels, llvmLibraryFile, eventlogEnabledUserEvents} = options
+    let CLProxyOptions{clOptions = clOPts@CLOptions{definitionFile, mainModuleName, port, logLevels, llvmLibraryFile, eventlogEnabledUserEvents}, koreSolverOptions} = options
         (logLevel, customLevels) = adjustLogLevels logLevels
         levelFilter :: Logger.LogSource -> LogLevel -> Bool
         levelFilter _source lvl =
@@ -98,7 +93,7 @@ main = do
                 mvarLogAction <- newMVar actualLogAction
                 let logAction = swappableLogger mvarLogAction
 
-                kore@KoreServer{runSMT} <- mkKoreServer Log.LoggerEnv{logAction} options
+                kore@KoreServer{runSMT} <- mkKoreServer Log.LoggerEnv{logAction} clOPts koreSolverOptions
 
                 withMDLib llvmLibraryFile $ \mdl -> do
                     mLlvmLibrary <- maybe (pure Nothing) (fmap Just . mkAPI) mdl
@@ -125,7 +120,7 @@ main = do
   where
     clParser =
         info
-            (clOptionsParser <**> versionInfoParser <**> helper)
+            (clProxyOptionsParser <**> versionInfoParser <**> helper)
             parserInfoModifiers
 
     withMDLib Nothing f = f Nothing
@@ -138,124 +133,24 @@ toSeverity LevelWarn = Just Log.Warning
 toSeverity LevelError = Just Log.Error
 toSeverity LevelOther{} = Nothing
 
-data CLOptions = CLOptions
-    { definitionFile :: FilePath
-    , mainModuleName :: Text
-    , llvmLibraryFile :: Maybe FilePath
-    , port :: Int
-    , logLevels :: [LogLevel]
-    , eventlogEnabledUserEvents :: [CustomUserEventType]
+data CLProxyOptions = CLProxyOptions
+    { clOptions :: CLOptions
     , koreSolverOptions :: !KoreSolverOptions
     }
 
 parserInfoModifiers :: InfoMod options
 parserInfoModifiers =
     fullDesc
-        <> header "Haskell Backend Booster - a JSON RPC server for quick symbolic execution of Kore definitions"
+        <> header "Haskell Backend Booster Proxy - a JSON RPC server combining kore and booster backends"
 
-versionInfoParser :: Parser (a -> a)
-versionInfoParser =
-    infoOption
-        versionInfoStr
-        ( short 'v'
-            <> long "version"
-            <> help "Print version info."
-        )
-
-clOptionsParser :: Parser CLOptions
-clOptionsParser =
-    CLOptions
-        <$> strArgument
-            ( metavar "DEFINITION_FILE"
-                <> help "Kore definition file to verify and use for execution"
-            )
-        <*> strOption
-            ( metavar "MODULE"
-                <> long "module"
-                <> help "The name of the main module in the Kore definition."
-            )
-        <*> optional
-            ( strOption
-                ( metavar "LLVM_BACKEND_LIBRARY"
-                    <> long "llvm-backend-library"
-                    <> help "Path to the .so/.dylib shared LLVM backend library"
-                )
-            )
-        <*> option
-            auto
-            ( metavar "SERVER_PORT"
-                <> long "server-port"
-                <> value 31337
-                <> help "Port for the RPC server to bind to"
-                <> showDefault
-            )
-        <*> many
-            ( option
-                (eitherReader readLogLevel)
-                ( metavar "LEVEL"
-                    <> long "log-level"
-                    <> short 'l'
-                    <> help
-                        ( "Log level: debug, info (default), warn, error, \
-                          \or a custom level from "
-                            <> show allowedLogLevels
-                        )
-                )
-            )
-        <*> many
-            ( option
-                (eitherReader readEventLogTracing)
-                ( metavar "TRACE"
-                    <> long "trace"
-                    <> short 't'
-                    <> help
-                        ( "Eventlog tracing options: "
-                            <> intercalate ", " [toKebab $ fromHumps $ show t | t <- [minBound .. maxBound] :: [CustomUserEventType]]
-                        )
-                )
-            )
+clProxyOptionsParser :: Parser CLProxyOptions
+clProxyOptionsParser =
+    CLProxyOptions
+        <$> clOptionsParser
         <*> parseKoreSolverOptions
-  where
-    readLogLevel :: String -> Either String LogLevel
-    readLogLevel = \case
-        "debug" -> Right LevelDebug
-        "info" -> Right LevelInfo
-        "warn" -> Right LevelWarn
-        "error" -> Right LevelError
-        other
-            | other `elem` allowedLogLevels -> Right (LevelOther $ pack other)
-            | otherwise -> Left $ other <> ": Unsupported log level"
 
-    readEventLogTracing :: String -> Either String CustomUserEventType
-    readEventLogTracing =
-        (\s -> maybe (Left $ s <> " not supported in eventlog tracing") Right $ readMaybe s) . toPascal . fromKebab
-
--- custom log levels that can be selected
-allowedLogLevels :: [String]
-allowedLogLevels = ["Rewrite"]
-
--- Partition provided log levels into standard and custom ones, and
--- select the lowest standard level. Default to 'LevelInfo' if no
--- standard log level was given.
-adjustLogLevels :: [LogLevel] -> (LogLevel, [LogLevel])
-adjustLogLevels ls = (standardLevel, customLevels)
-  where
-    (stds, customLevels) = partition (<= LevelError) ls
-    standardLevel = if null stds then LevelInfo else minimum stds
-
-versionInfoStr :: String
-versionInfoStr =
-    unlines
-        [ "hs-backend-booster version:"
-        , "  revision:\t" <> gitHash <> if gitDirty then " (dirty)" else ""
-        , "  branch:\t" <> fromMaybe "<unknown>" gitBranch
-        , "  last commit:\t" <> gitCommitDate
-        ]
-  where
-    VersionInfo{gitHash, gitDirty, gitBranch, gitCommitDate} = $versionInfo
-
-mkKoreServer :: Log.LoggerEnv IO -> CLOptions -> IO Proxy.KoreServer
-mkKoreServer loggerEnv@Log.LoggerEnv{logAction} CLOptions{definitionFile, mainModuleName, koreSolverOptions} =
+mkKoreServer :: Log.LoggerEnv IO -> CLOptions -> KoreSolverOptions -> IO KoreServer
+mkKoreServer loggerEnv@Log.LoggerEnv{logAction} CLOptions{definitionFile, mainModuleName} koreSolverOptions =
     flip Log.runLoggerT logAction $ do
         sd@GlobalMain.SerializedDefinition{internedTextCache} <-
             GlobalMain.deserializeDefinition
