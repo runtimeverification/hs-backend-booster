4f46d9e3071714f5329f1712b4ac458e4183dc90
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-03-19 20:22:43 +0000

    scripts/fourmolu: more descriptive

--- scripts/fourmolu.sh
+++ scripts/fourmolu.sh
@@ -1,1 +1,4 @@
-git ls-files | grep '.*\.hs$' | xargs ${FOURMOLU:-$(which fourmolu)} -o -XImportQualifiedPost -o -XTypeApplications -o -XPatternSynonyms -o -XBangPatterns -o -XOverloadedRecordDot -i
+#!/usr/bin/env bash
+set -euxo pipefail
+fourmolu=${FOURMOLU:-$(which fourmolu)} || { echo 'No fourmolu!' ; exit 1 ; }
+git ls-files | grep '.*\.hs$' | xargs ${fourmolu} -o -XImportQualifiedPost -o -XTypeApplications -o -XPatternSynonyms -o -XBangPatterns -o -XOverloadedRecordDot -i
