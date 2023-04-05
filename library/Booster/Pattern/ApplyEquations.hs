ddd5209c30e633608da3630c0fe505f118b7ce9f
Jost Berthold <jost.berthold@gmail.com>
2023-04-05 11:59:48 +1000

    WIP traversal code to apply equations from a theory

--- library/Booster/Pattern/ApplyEquations.hs
+++ library/Booster/Pattern/ApplyEquations.hs
@@ -12,12 +12,13 @@ import Control.Monad
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Except
 import Control.Monad.Trans.State
+import Data.Functor.Foldable
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Maybe (catMaybes, fromMaybe)
 
-import Booster.Definition.Base
 import Booster.Definition.Attributes.Base
+import Booster.Definition.Base
 import Booster.Pattern.Base
 import Booster.Pattern.Index
 import Booster.Pattern.Match
@@ -32,35 +33,76 @@ data EquationFailure a
     = IndexIsNone a
 
 data EquationState tag = EquationState
-    { theory :: Theory tag
+    { definition :: KoreDefinition
+    , theory :: Theory tag
     , hasChanged :: Bool
     , counter :: Int
     }
 
-startState :: Theory tag -> EquationState tag
-startState theory = EquationState{theory, hasChanged = False, counter = 0}
+startState :: KoreDefinition -> Theory tag -> EquationState tag
+startState definition theory =
+    EquationState{definition, theory, hasChanged = False, counter = 0}
 
-increment, markChanged, clearChanged :: EquationState tag -> EquationState tag
-increment s = s{counter = 1 + s.counter}
+increment, markChanged, clearChanged :: EquationM tag err ()
+increment = EquationM . modify $ \s -> s{counter = 1 + s.counter}
+markChanged = EquationM . modify $ \s -> s{hasChanged = True}
+clearChanged = EquationM . modify $ \s -> s{hasChanged = False}
 
-markChanged s = s{hasChanged = True}
-
-clearChanged s = s{hasChanged = False}
+getState :: EquationM tag err (EquationState tag)
+getState = EquationM get
 
 ----------------------------------------
-applyTermBottomUp ::
-    forall rule.
-    (KoreDefinition -> Theory rule) ->
-    Term ->
-    EquationM rule (EquationFailure Term) Term
-applyTermBottomUp selectTheory = undefined -- iterate $ cataA applyAtTopF ???
+applyTermBottomUp
+    , applyTermTopDown ::
+        forall rule.
+        Term ->
+        EquationM rule (EquationFailure Term) Term
+applyTermBottomUp =
+    cataA $ \case
+        DomainValueF s val ->
+            pure $ DomainValue s val
+        VarF var ->
+            pure $ Var var
+        InjectionF src trg t ->
+            Injection src trg <$> t -- no injection simplification
+        AndTermF arg1 arg2 ->
+            AndTerm <$> arg1 <*> arg2 -- no \and simplification
+        SymbolApplicationF sym sorts args -> do
+            t <- SymbolApplication sym sorts <$> sequence args
+            -- try to apply equations here
+            applyAtTop t
+applyTermTopDown = \case
+    dv@DomainValue{} ->
+        pure dv
+    v@Var{} ->
+        pure v
+    Injection src trg t ->
+        Injection src trg <$> applyTermTopDown t -- no injection simplification
+    AndTerm arg1 arg2 ->
+        AndTerm <$> applyTermTopDown arg1 <*> applyTermTopDown arg2 -- no \and simplification
+    app@(SymbolApplication sym sorts args) -> do
+        -- try to apply equations
+        t <- applyAtTop app
+        if (getAttributes t).hash /= (getAttributes app).hash
+            then do
+                markChanged -- or manage this inside applyAtTop?
+                case t of
+                    SymbolApplication sym' sorts' args' ->
+                        SymbolApplication sym' sorts' <$> mapM applyTermTopDown args'
+                    _otherwise ->
+                        applyTermTopDown t -- won't loop
+            else SymbolApplication sym sorts <$> mapM applyTermTopDown args
 
 -- TODO localise?
-applyAtTop :: forall rule. Theory rule -> Term -> EquationM rule (EquationFailure Term) Term
-applyAtTop theory term = do
+applyAtTop ::
+    forall rule.
+    Term ->
+    EquationM rule (EquationFailure Term) Term
+applyAtTop term = do
+    EquationState{definition, theory} <- getState
     let index = termTopIndex term
     when (index == None) $
-        throw $ IndexIsNone term
+        throw (IndexIsNone term)
     let idxEquations, anyEquations :: Map Priority [rule]
         idxEquations = fromMaybe Map.empty $ Map.lookup index theory
         anyEquations = fromMaybe Map.empty $ Map.lookup Anything theory
@@ -76,16 +118,24 @@ applyAtTop theory term = do
   where
     -- process one group of equations at a time, until something has happened
     processGroup :: [[rule]] -> EquationM rule (EquationFailure Term) Term
-    processGroup [] = pure term -- nothing to do, term stays the same
-    processGroups (eqs :: rest) = do
+    processGroup [] =
+        pure term -- nothing to do, term stays the same
+    processGroups (eqs : rest) = do
         -- try all equations in this group, and inspect the results
         results <- catMaybes <$> mapM (applyEquation term) eqs
-
-
-        undefined
+        case results of
+            [] ->
+                processGroups rest -- no success at all in this group
+            [newTerm] ->
+                pure newTerm -- single result
+            (new1 : _more) ->
+                -- choose first result if more than one FIXME should
+                -- be error for function equations (non-determinism)
+                pure new1
 
 applyEquation ::
     Term ->
     RewriteRule tag ->
     EquationM (RewriteRule tag) (EquationFailure Term) (Maybe Term)
-applyEquation = undefined
+applyEquation _term _rule =
+    pure Nothing
