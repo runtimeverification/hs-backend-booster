e3fcf9c7b4e659e6f7323ad27d7cfe8ef0bb7bc9
Georgy Lukyanov <mail@geo2a.info>
2023-06-27 12:54:22 +0200

    Remove `nixConfig` block from flake

--- flake.nix
+++ flake.nix
@@ -246,12 +246,4 @@
         '');
 
     };
-
-  nixConfig = {
-    extra-substituters =
-      [ "https://cache.iog.io"];
-    extra-trusted-public-keys = [
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-    ];
-  };
 }
