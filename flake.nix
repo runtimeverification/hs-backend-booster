c67dda20a412adfea279274211d985fbc43a05f0
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-07-12 15:14:08 +0000

    flake.nix: put dummy commit for testing

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     k-framework.url = "github:runtimeverification/k/v5.6.131";
-    haskell-backend.url = "github:runtimeverification/haskell-backend";
+    haskell-backend.url = "github:runtimeverification/haskell-backend/abcd";
     k-framework.inputs.booster-backend.follows = "";
     haskell-nix.follows = "haskell-backend/haskell-nix";
     nixpkgs.follows = "haskell-backend/haskell-nix/nixpkgs-unstable";
