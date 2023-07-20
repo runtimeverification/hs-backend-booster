304a54817682baeefe81728b41230754da12ad9a
Jost Berthold <jost.berthold@gmail.com>
2023-07-20 12:48:36 +1000

    use more types, general clean-up

--- tools/kore-json-differ/Main.hs
+++ tools/kore-json-differ/Main.hs
@@ -1,17 +1,22 @@
--- | (c) FIXME
+{- |
+Copyright   : (c) Runtime Verification, 2023
+License     : BSD-3-Clause
+-}
+
 module Main (main) where
 
 import Control.Applicative ((<|>))
 import Data.Aeson as Json
-import Data.Aeson.Encode.Pretty as Json
-import Data.Aeson.Types as Json
-import Data.Algorithm.Diff as Diff
-import Data.Algorithm.DiffOutput as Diff
+import Data.Aeson.Encode.Pretty (encodePretty')
+import Data.Aeson.Types (parseMaybe)
+import Data.Algorithm.Diff
+import Data.Algorithm.DiffOutput (ppDiff)
 import Data.ByteString.Lazy.Char8 qualified as BS
 import Data.Function (on)
 import Data.Maybe (fromMaybe)
 import Network.JSONRPC
-import System.Environment
+import System.Environment (getArgs)
+import System.Exit
 
 import Kore.JsonRpc.Types
 import Kore.Syntax.Json.Types hiding (Left, Right)
@@ -32,59 +37,113 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [] -> putStrLn usage
-        _ | "--help" `elem` args -> putStrLn usage
-        [x, y] -> diffJson x y >>= BS.putStrLn
-        _other -> putStrLn $ "ERROR: program requires exactly two arguments.\n\n" <> usage
+        [] ->
+            putStrLn usage
+        _
+            | "--help" `elem` args ->
+                putStrLn usage
+        [x, y] -> do
+            result <- diffJson x y
+            BS.putStrLn $ renderResult x y result
+            exitWith $ if isIdentical result then ExitSuccess else ExitFailure 1
+        _other -> do
+            putStrLn $ "ERROR: program requires exactly two arguments.\n\n" <> usage
+            exitWith $ ExitFailure 2
 
-diffJson :: FilePath -> FilePath -> IO BS.ByteString
+diffJson :: FilePath -> FilePath -> IO DiffResult
 diffJson korefile1 korefile2 = do
     contents1 <-
         decodeKoreRpc <$> BS.readFile korefile1
     contents2 <-
         decodeKoreRpc <$> BS.readFile korefile2
 
-    case (contents1, contents2) of
+    pure $ case (contents1, contents2) of
         (_, _)
             | contents1 == contents2 ->
+                Identical $ rpcTypeOf contents1
+        (NotRpcJson lines1, NotRpcJson lines2) -> do
+            TextDiff $ getGroupedDiff lines1 lines2
+        (other1, other2)
+            | rpcTypeOf other1 /= rpcTypeOf other2 ->
+                DifferentType (rpcTypeOf other1) (rpcTypeOf other2)
+            | otherwise -> do
+                JsonDiff (rpcTypeOf other1) $ computeJsonDiff other1 other2
+  where
+    computeJsonDiff =
+        getGroupedDiff `on` (BS.lines . encodePretty' rpcJsonConfig)
+
+{-
+    pure $ case (contents1, contents2) of
+        (_, _)
+            | contents1 == contents2 ->
+                Identic
+
                 pure . BS.unwords $
                     ["Files", BS.pack korefile1, "and", BS.pack korefile2, "are identical", typeString contents1 <> "s"]
-        (Garbled lines1, Garbled lines2) -> do
+        (NotRpcJson lines1, NotRpcJson lines2) -> do
             let result = getGroupedDiff lines1 lines2
-            pure $ "Both files contain garbled json." <> renderDiff result
+            pure $ "Both files do not contain json." <> renderDiff result
         (other1, other2)
-            | typeString other1 /= typeString other2 ->
-                pure . BS.unlines $
-                    [ "Json data in files is of different type"
-                    , "  * File " <> BS.pack korefile1 <> ": " <> typeString other1
-                    , "  * File " <> BS.pack korefile2 <> ": " <> typeString other2
-                    ]
             | otherwise -> do
                 let result = computeJsonDiff other1 other2
                 pure $ renderDiff result
+-}
+
+data DiffResult
+    = Identical KoreRpcType
+    | DifferentType KoreRpcType KoreRpcType
+    | JsonDiff KoreRpcType [Diff [BS.ByteString]]
+    | TextDiff [Diff [BS.ByteString]]
+    deriving (Eq, Show)
+
+isIdentical :: DiffResult -> Bool
+isIdentical Identical{} = True
+isIdentical _ = False
+
+renderResult :: FilePath -> FilePath -> DiffResult -> BS.ByteString
+renderResult korefile1 korefile2 = \case
+    Identical rpcType ->
+        BS.unwords
+            ["Files", file1, "and", file2, "are identical", typeString rpcType <> "s"]
+    DifferentType type1 type2 ->
+        BS.unlines
+            [ "Json data in files is of different type"
+            , "  * File " <> file1 <> ": " <> typeString type1
+            , "  * File " <> file2 <> ": " <> typeString type2
+            ]
+    JsonDiff rpcType diffs ->
+        BS.unlines
+            [ BS.unwords ["Files", file1, "and", file2, "are different", typeString rpcType <> "s"]
+            , renderDiff diffs
+            ]
+    TextDiff diffs ->
+        BS.unlines
+            [ BS.unwords ["Files", file1, "and", file2, "are different non-json files"]
+            , renderDiff diffs
+            ]
   where
-    computeJsonDiff =
-        getGroupedDiff `on` (BS.lines . Json.encodePretty' rpcJsonConfig)
+    file1 = BS.pack korefile1
+    file2 = BS.pack korefile2
 
 decodeKoreRpc :: BS.ByteString -> KoreRpcJson
 decodeKoreRpc input =
-    fromMaybe (Garbled splitInput) $
+    fromMaybe (NotRpcJson splitInput) $
         try [rpcRequest, rpcResponse, rpcError, koreJson, unknown]
   where
     try = foldl1 (<|>)
     rpcRequest = fmap RpcRequest $ do
         req <- Json.decode @Request input
         parser <- parseParams req.getReqMethod
-        Json.parseMaybe parser req.getReqParams
+        parseMaybe parser req.getReqParams
     rpcResponse = fmap RpcResponse $ do
         resp <- Json.decode @Response input
         foldl1
             (<|>)
-            [ Execute <$> Json.parseMaybe (Json.parseJSON @ExecuteResult) resp.getResult
-            , Implies <$> Json.parseMaybe (Json.parseJSON @ImpliesResult) resp.getResult
-            , Simplify <$> Json.parseMaybe (Json.parseJSON @SimplifyResult) resp.getResult
-            , AddModule <$> Json.parseMaybe (Json.parseJSON @()) resp.getResult
-            , GetModel <$> Json.parseMaybe (Json.parseJSON @GetModelResult) resp.getResult
+            [ Execute <$> parseMaybe (Json.parseJSON @ExecuteResult) resp.getResult
+            , Implies <$> parseMaybe (Json.parseJSON @ImpliesResult) resp.getResult
+            , Simplify <$> parseMaybe (Json.parseJSON @SimplifyResult) resp.getResult
+            , AddModule <$> parseMaybe (Json.parseJSON @()) resp.getResult
+            , GetModel <$> parseMaybe (Json.parseJSON @GetModelResult) resp.getResult
             ]
     rpcError =
         Json.decode @ErrorObj input
@@ -95,7 +154,7 @@ decodeKoreRpc input =
     koreJson =
         RpcKoreJson <$> Json.decode @KoreJson input
     unknown =
-        RpcUnknown <$> Json.decode @Object input
+        RpcJson <$> Json.decode @Object input
     -- last resort: break the bytestring into lines at json-relevant
     -- characters (ignoring quoting)
     splitInput = BS.splitWith (`elem` [':', ',', '{', '}']) input
@@ -106,8 +165,8 @@ data KoreRpcJson
     | RpcResponse (API 'Res)
     | RpcError String Int Value
     | RpcKoreJson KoreJson
-    | RpcUnknown Object
-    | Garbled [BS.ByteString]
+    | RpcJson Object
+    | NotRpcJson [BS.ByteString]
     deriving stock (Eq, Show)
 
 instance ToJSON KoreRpcJson where
@@ -123,17 +182,47 @@ instance ToJSON KoreRpcJson where
         RpcResponse r -> toJSON r
         RpcError msg code v -> toJSON (msg, code, v)
         RpcKoreJson t -> toJSON t
-        RpcUnknown v -> toJSON v
-        Garbled bs -> toJSON $ map BS.unpack bs
+        RpcJson v -> toJSON v
+        NotRpcJson bs -> toJSON $ map BS.unpack bs
 
-typeString :: KoreRpcJson -> BS.ByteString
-typeString = \case
-    RpcRequest _ -> "request"
-    RpcResponse _ -> "response"
-    RpcError{} -> "error response"
-    RpcKoreJson _ -> "Kore term"
-    RpcUnknown _ -> "unknown object"
-    Garbled _ -> "garbled json"
+-- | Information about the kind of object in 'KoreRpcJson' ()without payload)
+data KoreRpcType
+    = RpcReq APIMethod
+    | RpcResp APIMethod
+    | RpcErr
+    | RpcKore
+    | RpcJs
+    | NotRpcJs
+    deriving (Eq, Ord)
+
+instance Show KoreRpcType where
+    show = \case
+        RpcReq method -> show method <> " request"
+        RpcResp method -> show method <> " response"
+        RpcErr -> "error response"
+        RpcKore -> "Kore term"
+        RpcJs -> "unknown object"
+        NotRpcJs -> "non-JSON file"
+
+typeString :: KoreRpcType -> BS.ByteString
+typeString = BS.pack . show
+
+rpcTypeOf :: KoreRpcJson -> KoreRpcType
+rpcTypeOf = \case
+    RpcRequest req -> RpcReq $ methodOf req
+    RpcResponse r -> RpcResp $ methodOf r
+    RpcError{} -> RpcErr
+    RpcKoreJson{} -> RpcKore
+    RpcJson{} -> RpcJs
+    NotRpcJson{} -> NotRpcJs
+  where
+    methodOf = \case
+        Execute _ -> ExecuteM
+        Implies _ -> ImpliesM
+        Simplify _ -> SimplifyM
+        AddModule _ -> AddModuleM
+        GetModel _ -> GetModelM
+        Cancel -> error "Cancel"
 
 -------------------------------------------------------------------
 -- pretty diff output
