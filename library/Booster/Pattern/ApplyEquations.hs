e45c398c602dfd3ccb0c65e51ea559255cb68622
Sam Balco <goodlyrottenapple@gmail.com>
2023-08-15 16:16:42 +0100

    Revert "merge main"
    This reverts commit 0b1dc0b7e1d4a8bce20776af3c839f786b27eb33, reversing
    changes made to 995c58e7d67ffb30d43aa1f7638b79438208b7f6.

--- library/Booster/Pattern/ApplyEquations.hs
+++ library/Booster/Pattern/ApplyEquations.hs
@@ -62,12 +62,6 @@ newtype EquationT io a
 throw :: MonadLoggerIO io => EquationFailure -> EquationT io a
 throw = EquationT . lift . throwE
 
-catch_ ::
-    MonadLoggerIO io => EquationT io a -> (EquationFailure -> EquationT io a) -> EquationT io a
-catch_ (EquationT op) hdlr = EquationT $ do
-    cfg <- ask
-    lift (runReaderT op cfg `catchE` (\e -> let EquationT fallBack = hdlr e in runReaderT fallBack cfg))
-
 data EquationFailure
     = IndexIsNone Term
     | TooManyIterations Int Term Term
@@ -75,25 +69,6 @@ data EquationFailure
     | SideConditionsFalse [Predicate]
     | InternalError Text
     deriving stock (Eq, Show)
-
-instance Pretty EquationFailure where
-    pretty = \case
-        IndexIsNone t ->
-            "Index 'None' for term " <> pretty t
-        TooManyIterations count start end ->
-            vsep
-                [ "Unable to finish evaluation in " <> pretty count <> " iterations"
-                , "Started with: " <> pretty start
-                , "Stopped at: " <> pretty end
-                ]
-        EquationLoop ts ->
-            vsep $ "Evaluation produced a loop:" : map pretty ts
-        SideConditionsFalse ps ->
-            vsep $
-                "Side conditions were found to be false during evaluation (pruning)"
-                    : map pretty ps
-        InternalError msg ->
-            "Internal error during evaluation: " <> pretty msg
 
 data EquationConfig = EquationConfig
     { definition :: KoreDefinition
@@ -315,7 +290,7 @@ evaluatePattern' Pattern{term, constraints, ceilConditions} = do
     simplifyAssumedPredicate p = do
         allPs <- predicates <$> getState
         let otherPs = Set.delete p allPs
-        EquationT $ lift $ lift $ modify $ \s -> s{termStack = [], predicates = otherPs}
+        EquationT $ lift $ lift $ modify $ \s -> s{predicates = otherPs}
         newP <- simplifyConstraint' p
         pushConstraints [newP]
 
@@ -574,32 +549,14 @@ applyEquation term rule = fmap (either id Success) $ runExceptT $ do
                                     lift $ pushConstraints conditions
                             pure $ substituteInTerm subst rule.rhs
   where
-    -- Simplify given predicate in a nested EquationT execution.
-    -- Return Nothing immediately if it is Bottom, return (Just
-    -- Nothing) if it is Top, otherwise return (Just simplified).
+    -- evaluate/simplify a predicate, cut the operation short when it
+    -- is Bottom.
     checkConstraint ::
         Predicate ->
         MaybeT (ExceptT ApplyEquationResult (EquationT io)) (Maybe Predicate)
     checkConstraint p = do
-        lift . logOther (LevelOther "Simplify") $
-            "Recursive simplification of predicate: " <> pack (renderDefault (pretty p))
-        oldChangeFlag <- lift $ lift getChanged
-        let restoreChangeFlag :: EquationT io ()
-            restoreChangeFlag =
-                if oldChangeFlag
-                    then setChanged
-                    else resetChanged
-            fallBackToP :: EquationFailure -> EquationT io Predicate
-            fallBackToP e = do
-                logOther (LevelOther "Simplify") . pack . renderDefault $
-                    "Aborting recursive simplification:" <> pretty e
-                pure p
-        -- exceptions need to be handled differently in the recursion,
-        -- falling back to the unsimplified constraint instead of aborting.
-        simplified <-
-            lift . lift $
-                resetChanged >> simplifyConstraint' p `catch_` fallBackToP <* restoreChangeFlag
-        case simplified of
+        mApi <- (.llvmApi) <$> lift (lift getConfig)
+        case simplifyPredicate mApi p of
             Predicate FalseBool -> fail "side condition was false"
             Predicate TrueBool -> pure Nothing
             other -> pure $ Just other
