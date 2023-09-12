239e6f24f83bb1c34a637d3f1a0022446bab0e62
Sam Balco <goodlyrottenapple@gmail.com>
2023-09-12 16:35:39 +0100

    can now internalise and compare the bug report runs for equal length and exact reposnse equality

--- tools/tarball-compare/Main.hs
+++ tools/tarball-compare/Main.hs
@@ -22,7 +22,6 @@ import Codec.Archive.Tar qualified as Tar
 import Codec.Archive.Tar.Check qualified as Tar
 import Codec.Compression.BZip qualified as BZ2
 import Codec.Compression.GZip qualified as GZip
-import Control.Exception
 import Control.Monad.Trans.Except
 import Data.Aeson qualified as Json
 import Data.Aeson.KeyMap qualified as Json
@@ -31,13 +30,97 @@ import Data.List.Extra
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Maybe (fromMaybe)
-import Options.Applicative
 import System.FilePath
 
 import Kore.JsonRpc.Types
+import System.Environment (getArgs)
+import Control.Monad (forM_)
+import Control.Monad.Extra (when)
+
+data BugReportData = BugReportData {
+    requests :: Map FilePath BS.ByteString
+  , responses :: Map FilePath BS.ByteString
+} deriving Show
+
+instance Semigroup BugReportData where
+    (BugReportData req1 resp1) <> (BugReportData req2 resp2) = BugReportData (req1 <> req2) (resp1 <> resp2)
+
+instance Monoid BugReportData where
+    mempty = BugReportData mempty mempty
+
+
+data BugReportDiff = BugReportDiff {
+    booster :: BugReportData,
+    koreRpc :: BugReportData
+} deriving Show
+
+instance Semigroup BugReportDiff where
+    (BugReportDiff b1 k1) <> (BugReportDiff b2 k2) = BugReportDiff (b1 <> b2) (k1 <> k2)
+
+instance Monoid BugReportDiff where
+    mempty = BugReportDiff mempty mempty
+
 
 main :: IO ()
-main = undefined
+main = getArgs >>= \case
+    [tarFile] -> do
+        contents <- Tar.checkSecurity . unpack tarFile <$> BS.readFile tarFile
+        case unpackBugReports contents of
+            Left err -> either print print err
+            Right bugReports -> forM_ (Map.toList bugReports) $ \(file, BugReportDiff{booster, koreRpc}) -> do
+                putStrLn $ "------------- " <> file <> " -------------"
+                if null $ Map.keys booster.requests then
+                    putStrLn $ "No Booster data... skipping..."
+                    else do
+                        when (Map.keys koreRpc.requests /= Map.keys booster.requests || Map.keys koreRpc.responses /= Map.keys booster.responses) $ 
+                            putStrLn "Booster and kore-rpc have different requests/responses"
+                        forM_ (Map.toList koreRpc.requests) $ \(koreRpcReqKey, koreRpcReqJson) -> do
+                            case Map.lookup koreRpcReqKey booster.requests of
+                                Nothing -> putStrLn $ "Request " <> koreRpcReqKey <> " does not exist in booster"
+                                Just boosterReqJson ->
+                                    when (koreRpcReqJson /= boosterReqJson) $
+                                        putStrLn $ "Request " <> koreRpcReqKey <> " differs in booster"
+                            case (Map.lookup koreRpcReqKey koreRpc.responses, Map.lookup koreRpcReqKey booster.responses) of
+                                (Just koreResp, Just boosterResp) ->
+                                    when (koreResp /= boosterResp) $
+                                        putStrLn $ "Response " <> koreRpcReqKey <> " differs in booster"
+                                (Just _, Nothing) -> putStrLn $ "Response " <> koreRpcReqKey <> " missing in booster"
+                                (Nothing, Just _) -> putStrLn $ "Response " <> koreRpcReqKey <> " missing in kore-rpc"
+                                (Nothing, Nothing) -> putStrLn $ "Response " <> koreRpcReqKey <> " missing"
+
+    _ -> putStrLn "incorrect args"
+    where
+        unpack tarFile
+            | ".tar" == takeExtension tarFile = Tar.read
+            | ".tgz" == takeExtension tarFile = Tar.read . GZip.decompress
+            | ".tar.gz" `isSuffixOf` takeExtensions tarFile = Tar.read . GZip.decompress
+            | ".tar.bz2" `isSuffixOf` takeExtensions tarFile = Tar.read . BZ2.decompress
+            | otherwise = Tar.read
+
+
+unpackBugReports :: 
+    Tar.Entries (Either Tar.FormatError Tar.FileNameError) ->
+    Either (Either Tar.FormatError Tar.FileNameError) (Map FilePath BugReportDiff)
+unpackBugReports = Tar.foldEntries unpackBugReportData (Right mempty) Left
+    where
+        unpackBugReportData ::
+            Tar.Entry ->
+            Either (Either Tar.FormatError Tar.FileNameError) (Map FilePath BugReportDiff) ->
+            Either (Either Tar.FormatError Tar.FileNameError) (Map FilePath BugReportDiff)
+        unpackBugReportData _ err@(Left _) = err
+        unpackBugReportData entry acc@(Right m)
+            | Tar.NormalFile bs _size <- Tar.entryContent entry
+            , ".tar" `isSuffixOf` file
+            , contents <- Tar.read bs = 
+                case unpackBugReportDataFrom contents of
+                    Left err -> Left $ Left err
+                    Right bugReport -> Right $ Map.alter (insertBugReport bugReport) file m    
+            | otherwise = acc
+         where
+            (dir, file) = splitFileName (Tar.entryPath entry)
+            insertBugReport b bDiff = Just $ (\bugReportDiff -> if "booster" `isInfixOf` dir 
+                    then bugReportDiff {booster = b}
+                    else bugReportDiff {koreRpc = b}) $ fromMaybe mempty bDiff
 
 {- Unpack all files inside rpc_* directories in a tarball, into maps
    of file prefixes (numbers) to requests and resp. responses.
@@ -45,41 +128,30 @@ main = undefined
    Assumes there is a single rpc_* directory in the tarball (fails on
    duplicate file base names).
 -}
-unpackRpcFrom ::
-    FilePath ->
-    IO (Map FilePath BS.ByteString, Map FilePath BS.ByteString)
-unpackRpcFrom tarFile = do
-    contents <- Tar.checkSecurity . unpack <$> BS.readFile tarFile
-    Tar.foldEntries unpackRpc (pure (Map.empty, Map.empty)) throwAnyError contents
+unpackBugReportDataFrom ::
+    Tar.Entries err ->
+    Either err BugReportData
+unpackBugReportDataFrom = Tar.foldEntries unpackRpc (Right mempty) Left
   where
-    unpack
-        | ".tar" == takeExtension tarFile = Tar.read
-        | ".tgz" == takeExtension tarFile = Tar.read . GZip.decompress
-        | ".tar.gz" `isSuffixOf` takeExtensions tarFile = Tar.read . GZip.decompress
-        | ".tar.bz2" `isSuffixOf` takeExtensions tarFile = Tar.read . BZ2.decompress
-        | otherwise = Tar.read
-
-    throwAnyError :: Either Tar.FormatError Tar.FileNameError -> IO a
-    throwAnyError = either throwIO throwIO
-
     unpackRpc ::
         Tar.Entry ->
-        IO (Map FilePath BS.ByteString, Map FilePath BS.ByteString) ->
-        IO (Map FilePath BS.ByteString, Map FilePath BS.ByteString)
-    unpackRpc entry acc
+        Either err BugReportData ->
+        Either err BugReportData
+    unpackRpc _ err@(Left _) = err
+    unpackRpc entry acc@(Right BugReportData{requests, responses})
         | Tar.NormalFile bs _size <- Tar.entryContent entry
         , "rpc_" `isPrefixOf` dir
-        , ".json" `isSuffixOf` file = do
-            (reqMap, respMap) <- acc
-            let (isRequest, number, contents)
+        , ".json" `isSuffixOf` file =
+            let (isRequest, number, json)
                     | Just num <- stripSuffix requestSuffix file =
                         (True, num, bs)
                     | Just num <- stripSuffix responseSuffix file =
                         (False, num, bs)
                     | otherwise = error $ "Bad file in tarball: " <> show (dir </> file)
-            if isRequest
-                then pure (Map.insert number contents reqMap, respMap)
-                else pure (reqMap, Map.insert number contents respMap)
+            in
+                Right $ if isRequest
+                    then BugReportData {requests = Map.insert number json requests, responses}
+                    else BugReportData {requests, responses = Map.insert number json responses}
         | otherwise = acc
       where
         (dir, file) = splitFileName (Tar.entryPath entry)
