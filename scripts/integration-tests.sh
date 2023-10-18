635440cde9c4ade910f37931bb3ec1ee05e313f4
Sam Balco <goodlyrottenapple@gmail.com>
2023-10-18 16:02:50 +0100

    fix up script

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -9,6 +9,7 @@ cabal test llvm-integration
 
 # The runDirectoryTest.sh script expects the following env vars to be set
 export PLUGIN_DIR=$(nix build github:runtimeverification/blockchain-k-plugin/$PLUGIN_VERSION --no-link --json | jq -r '.[].outputs | to_entries[].value')
+cabal build all
 KORE_RPC_BOOSTER=$(cabal exec which kore-rpc-booster)
 BOOSTER_DEV=$(cabal exec which booster-dev)
 KORE_RPC_DEV=$(cabal exec which kore-rpc-dev)
