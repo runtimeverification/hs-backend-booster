1bf9106c42d890153a3637e165e7f075047ba799
Sam Balco <goodlyrottenapple@gmail.com>
2023-10-20 10:24:30 +0100

    replace llvm-backend-matching with llvm-kompile-matching

--- test/rpc-integration/generateDirectoryTest.sh
+++ test/rpc-integration/generateDirectoryTest.sh
@@ -45,7 +45,7 @@ cat >>resources/$NAME.kompile <<EOL
 cp $NAME.haskell.kore $NAME.kore
 
 # Regenerate llvm backend decision tree
-llvm-backend-matching $NAME.llvm.kore qbaL ./dt 0
+llvm-kompile-matching $NAME.llvm.kore qbaL ./dt 0
 
 # kompile llvm-definition to interpreter
 
