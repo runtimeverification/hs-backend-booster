4d41d7cb15129fb6d2ba6a4f06f000f927690ff0
Georgy Lukyanov <mail@geo2a.info>
2023-11-08 08:42:04 +0100

    Make modifications to kevm performance test script

--- scripts/performance-tests-kevm.sh
+++ scripts/performance-tests-kevm.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-KEVM_VERSION='v1.0.335'
+# Disable the Python keyring, otherwise poetry sometimes asks for password. See
+#  https://github.com/pypa/pip/issues/7883
+export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+
+KEVM_VERSION=${KEVM_VERSION:-'v1.0.335'}
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 MASTER_COMMIT="$(git rev-parse origin/main)"
 
-FEATURE_BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
+FEATURE_BRANCH_NAME=${FEATURE_BRANCH_NAME:-"$(git rev-parse --abbrev-ref HEAD)"}
 FEATURE_BRANCH_NAME="${FEATURE_BRANCH_NAME//\//-}"
+
+PYTEST_PARALLEL=${PYTEST_PARALLEL:-2}
 
 if [[ $FEATURE_BRANCH_NAME == "master" ]]; then
   FEATURE_BRANCH_NAME="feature"
@@ -23,10 +29,9 @@ if [ ! -e "$TEMPD" ]; then
     exit 1
 fi
 
-# Make sure the temp directory gets removed on script exit.
+# Make sure the temp directory gets removed and kore-rpc-booster gets killed on script exit.
 trap "exit 1"           HUP INT PIPE QUIT TERM
-trap 'rm -rf "$TEMPD"'  EXIT
-
+trap 'rm -rf "$TEMPD" && killall kore-rpc-booster'  EXIT
 
 feature_shell() {
   GC_DONT_GC=1 nix develop . --extra-experimental-features 'nix-command flakes' --override-input k-framework/booster-backend $SCRIPT_DIR/../ --command bash -c "$1"
@@ -45,10 +50,10 @@ git submodule update --init --recursive --depth 1 kevm-pyk/src/kevm_pyk/kproj/pl
 
 feature_shell "make poetry && poetry run -C kevm-pyk -- kevm-dist --verbose build plugin haskell --jobs 4"
 
-feature_shell "make test-prove-pyk PYTEST_PARALLEL=8 PYTEST_ARGS='--timeout 7200 -vv --use-booster' > $SCRIPT_DIR/kevm-$KEVM_VERSION-$FEATURE_BRANCH_NAME.log"
+feature_shell "make test-prove-pyk PYTEST_PARALLEL=$PYTEST_PARALLEL PYTEST_ARGS='--maxfail=0 --timeout 7200 -vv --use-booster' > $SCRIPT_DIR/kevm-$KEVM_VERSION-$FEATURE_BRANCH_NAME.log"
 
 if [ ! -e "$SCRIPT_DIR/kevm-$KEVM_VERSION-master-$MASTER_COMMIT.log" ]; then
-  master_shell "make test-prove-pyk PYTEST_PARALLEL=8 PYTEST_ARGS='--timeout 7200 -vv --use-booster' > $SCRIPT_DIR/kevm-$KEVM_VERSION-master-$MASTER_COMMIT.log"
+  master_shell "make test-prove-pyk PYTEST_PARALLEL=$PYTEST_PARALLEL PYTEST_ARGS='--maxfail=0 --timeout 7200 -vv --use-booster' > $SCRIPT_DIR/kevm-$KEVM_VERSION-master-$MASTER_COMMIT.log"
 fi
 
 cd $SCRIPT_DIR
