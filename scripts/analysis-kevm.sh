5fe5f146bbe3d1118f84a566618e3ab9c20dd6c5
Sam Balco <goodlyrottenapple@gmail.com>
2024-01-15 18:24:28 +0000

    fix dir

--- scripts/analysis-kevm.sh
+++ scripts/analysis-kevm.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-export KEVM_VERSION=${KEVM_VERSION:-'master'}
-
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-
-FEATURE_BRANCH_NAME=${FEATURE_BRANCH_NAME:-"$(git rev-parse --abbrev-ref HEAD)"}
-FEATURE_BRANCH_NAME="${FEATURE_BRANCH_NAME//\//-}"
-
-if [[ $FEATURE_BRANCH_NAME == "master" ]]; then
-  FEATURE_BRANCH_NAME="feature"
-fi
 
 export PYTEST_PARALLEL=8 
 
 BUG_REPORT_DIR=$($SCRIPT_DIR/performance-tests-kevm.sh --bug-report)
+
+echo "Bug reports saved at $BUG_REPORT_DIR"
 
 nix_shell() {
   GC_DONT_GC=1 nix develop $SCRIPT_DIR/..#cabal --extra-experimental-features 'nix-command flakes' --command bash -c "$1"
@@ -28,17 +21,17 @@ nix_shell "cabal build kore-rpc-client"
 
 export CLIENT=$(nix_shell "cabal exec which kore-rpc-client")
 
-export LLVM_LIB="$SCRIPT_DIR/bug-reports/kevm-$KEVM_VERSION-$FEATURE_BRANCH_NAME/interpreter.dylib"
+export LLVM_LIB="$BUG_REPORT_DIR/interpreter.dylib"
 
 export LOG_DIR="$BUG_REPORT_DIR"
 
-for tar in $(find $SCRIPT_DIR/bug-reports/kevm-$KEVM_VERSION-$FEATURE_BRANCH_NAME -name \*tar ); 
+for tar in $(find $BUG_REPORT_DIR -name \*tar ); 
 do 
   echo "######## $tar ########"; 
-  nix_shell "$SCRIPT_DIR/run-with-tarball.sh $tar -l Aborts --print-stats 2>&1 | tee $SCRIPT_DIR/bug-reports/kevm-$KEVM_VERSION-$FEATURE_BRANCH_NAME/$(basename $tar).out"; 
+  nix_shell "$SCRIPT_DIR/run-with-tarball.sh $tar -l Aborts --print-stats 2>&1 | tee $BUG_REPORT_DIR/$(basename $tar).out"; 
 done
 
-cd $SCRIPT_DIR/bug-reports/kevm-$KEVM_VERSION-$FEATURE_BRANCH_NAME
+cd $BUG_REPORT_DIR
 
 # Counting abort reasons
 grep -e "Uncertain about" *log | sed -e 's,/tmp/tmp.[^/]*/evm-semantics/kevm-pyk/src/kevm_pyk/kproj/evm-semantics/,,' -e "s/.*Uncertain about \(.*\)$/\1/" -e "s/jumpi.true :.*/jumpi.true/" | nix_shell "runghc $SCRIPT_DIR/Count.hs" > abort_reasons.count
