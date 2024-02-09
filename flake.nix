f072b4f641c5f2cc801d7c9cc43eab1443eb1d14
rv-jenkins <devops@runtimeverification.com>
2024-02-09 15:15:55 +0000

    update haskell-backend to version c6b45490b90df0763c67c79c62e87b769aabb47c

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "hs-backend-booster";
 
   inputs = {
-    haskell-backend.url = "github:runtimeverification/haskell-backend/7e2b8b363bff6f6a2391191fe5e04d9337ace11b";
+    haskell-backend.url = "github:runtimeverification/haskell-backend/c6b45490b90df0763c67c79c62e87b769aabb47c";
     stacklock2nix.follows = "haskell-backend/stacklock2nix";
     nixpkgs.follows = "haskell-backend/nixpkgs";
   };
