7e5c398e24945fb848b8260312c47f339bb763e9
Jost Berthold <jost.berthold@gmail.com>
2024-02-13 14:09:47 +1100

    kmap helper in unit tests

--- unit-tests/Test/Booster/Pattern/Match.hs
+++ unit-tests/Test/Booster/Pattern/Match.hs
@@ -227,29 +227,18 @@ kmapTerms =
             "One key and rest variable ~= two keys (one the same): Match rest with other key singleton"
             concreteKMapWithOneItemAndRest
             concreteKMapWithTwoItems
-            ( let restMap =
-                    KMap
-                        testKMapDefinition
-                        [(dv kmapKeySort "key2", dv kmapElementSort "value2")]
-                        Nothing
+            ( let restMap = kmap [(dv kmapKeySort "key2", dv kmapElementSort "value2")] Nothing
                in success [("REST", kmapSort, restMap)]
             )
         , -- pattern has more assocs than subject
-          let patRest =
-                KMap
-                    testKMapDefinition
-                    [(dv kmapKeySort "key2", dv kmapElementSort "value2")]
-                    Nothing
+          let patRest = kmap [(dv kmapKeySort "key2", dv kmapElementSort "value2")] Nothing
            in test
                 "Extra concrete key in pattern, no rest in subject: fail on rest"
                 concreteKMapWithTwoItems
                 concreteKMapWithOneItem
                 (failed $ DifferentValues patRest emptyKMap)
         , let patRest =
-                KMap
-                    testKMapDefinition
-                    [(dv kmapKeySort "key2", dv kmapElementSort "value2")]
-                    Nothing
+                kmap [(dv kmapKeySort "key2", dv kmapElementSort "value2")] Nothing
            in test
                 "Extra concrete key in pattern, opaque rest in subject: indeterminate part"
                 concreteKMapWithTwoItems
@@ -263,20 +252,14 @@ kmapTerms =
             ( success [("A", kmapKeySort, dv kmapKeySort "key2")]
             )
         , let patMap =
-                KMap
-                    testKMapDefinition
-                    [(var "K" kmapKeySort, var "V" kmapElementSort)]
-                    (Just $ var "PATTERN" kmapSort)
+                kmap [(var "K" kmapKeySort, var "V" kmapElementSort)] (Just "PATTERN")
            in test
                 "Variable key ~= concrete key with rest in subject and pattern: indeterminate"
                 patMap
                 functionKMapWithOneItemAndRest
                 (MatchIndeterminate patMap functionKMapWithOneItemAndRest)
         , let patMap =
-                KMap
-                    testKMapDefinition
-                    [(var "K" kmapKeySort, var "V" kmapElementSort)]
-                    (Just $ var "PATTERN" kmapSort)
+                kmap [(var "K" kmapKeySort, var "V" kmapElementSort)] (Just "PATTERN")
            in test
                 "Variable key and opaque rest ~= two items: indeterminate"
                 patMap
@@ -288,25 +271,27 @@ kmapTerms =
             functionKMapWithOneItem
             (failed $ DifferentValues concreteKMapWithOneItemAndRest functionKMapWithOneItem)
         , let patMap =
-                KMap
-                    testKMapDefinition
+                kmap
                     [ (var "A" kmapKeySort, dv kmapElementSort "a")
                     , (var "B" kmapKeySort, dv kmapElementSort "b")
                     ]
-                    (Just $ var "PATTERN" kmapSort)
+                    (Just "PATTERN")
               subjMap =
-                KMap
-                    testKMapDefinition
+                kmap
                     [ (dv kmapKeySort "k1", dv kmapElementSort "a")
                     , (dv kmapKeySort "k2", dv kmapElementSort "b")
                     ]
-                    (Just $ var "SUBJECT" kmapSort)
+                    (Just "SUBJECT")
            in test
                 "Disjoint non-singleton maps, non-concrete keys in pattern: indeterminate"
                 patMap
                 subjMap
                 (MatchIndeterminate patMap subjMap)
         ]
+  where
+    kmap :: [(Term, Term)] -> Maybe VarName -> Term
+    kmap assocs mbRestVar =
+        KMap testKMapDefinition assocs $ fmap (`var` kmapSort) mbRestVar
 
 cornerCases :: TestTree
 cornerCases =
